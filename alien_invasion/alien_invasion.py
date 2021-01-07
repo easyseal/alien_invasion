67615368a2c79ad29d06d0d565aadb6e351e5dad
easyseal <easyseal@163.com>
2021-01-07 09:39:21 +0800

    pygame

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -1,11 +1,12 @@
 import sys
 import pygame
+from settings import Settings
 def run_game():
     # 初始化游戏并创建一个屏幕对象
     pygame.init()
-    screen = pygame.display.set_mode((1200,800))
+    ai_settings = Settings()
+    screen = pygame.display.set_mode((ai_settings.screen_width,ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
-    bg_color = (230,230,230)
     # 开始游戏的主循环
     while True:
         # 监视键盘和鼠标事件
@@ -13,7 +14,7 @@ def run_game():
             if event.type == pygame.QUIT:
                 sys.exit()
         # 设置背景色
-        screen.fill(bg_color)
+        screen.fill(ai_settings.bg_color)
         # 让最近绘制的屏幕可见
         pygame.display.flip()
 
