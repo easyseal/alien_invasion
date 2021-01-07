1b48acee753482c8e1849b7cbd589aac6a1c42f8
easyseal <easyseal@163.com>
2021-01-07 14:45:04 +0800

    pygame

--- alien_invasion/game_functions.py
+++ alien_invasion/game_functions.py
@@ -27,6 +27,7 @@ def update_screen(ai_settings,screen,ship):
     """更新屏幕上的图像，并切换到新屏幕"""
     # 每次循环时都重绘屏幕
     screen.fill(ai_settings.bg_color)
+    # 飞船绘制
     ship.blitme()
     # 让最近绘制的屏幕可见
     pygame.display.flip()
